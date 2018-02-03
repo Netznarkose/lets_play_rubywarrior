3d3ac3dc94ba04844dab32b2878892dad0e2e591
max <mmaintz@posteo.de>
2018-02-03 21:24:55 +0100

    level 4

--- rubywarrior/annemarie-beginner/player.rb
+++ rubywarrior/annemarie-beginner/player.rb
@@ -3,10 +3,14 @@ class Player
   def play_turn(warrior)
     if warrior.feel.enemy?
       warrior.attack!
-    elsif !warrior.feel.enemy?  && warrior.health < 20
+    elsif !warrior.feel.enemy?  && warrior.health < 20 && !damage?(warrior)
       warrior.rest!
     else
       warrior.walk!
     end
+    @health = warrior.health
+  end
+  def damage?(warrior)
+    warrior.health < @health
   end
 end
