6aa69d1189eae633ed241a47ce47b6d8a51126df
max <mmaintz@posteo.de>
2018-02-04 13:36:29 +0100

    level 6

--- rubywarrior/annemarie-beginner/player.rb
+++ rubywarrior/annemarie-beginner/player.rb
@@ -1,16 +1,40 @@
 require 'pry-byebug'
 class Player
+  def initialize
+    @health = 20
+    @captive_rescued = false
+  end
+
   def play_turn(warrior)
-    if warrior.feel.enemy?
-      warrior.attack!
-    elsif warrior.feel.captive?
-      warrior.rescue!
-    elsif !warrior.feel.enemy?  && warrior.health < 20 && !damage?(warrior)
+    if @captive_rescued == false
+      funky_moves(warrior, :backward)
+    else
+      funky_moves(warrior, :forward)
+    end
+  end
+
+  def funky_moves(warrior, direction)
+    if under_attack_and_in_bad_shape(warrior)
+      warrior.walk!(:backward)
+    elsif warrior.feel(direction).captive?
+      warrior.rescue!(direction)
+      @captive_rescued = true
+    elsif warrior.feel(direction).enemy?
+      warrior.attack!(direction)
+    elsif not_attacked_no_enemy_around_and_health_under_max(warrior)
       warrior.rest!
     else
-      warrior.walk!
+      warrior.walk!(direction)
     end
     @health = warrior.health
+  end
+
+  def under_attack_and_in_bad_shape(warrior)
+    damage?(warrior) && warrior.health < 12
+  end
+
+  def not_attacked_no_enemy_around_and_health_under_max(warrior)
+    !warrior.feel.enemy? && warrior.health < 20 && !damage?(warrior)
   end
   def damage?(warrior)
     warrior.health < @health
