98714be2e99dd4548c5db6ca717d7d459f445e47
max <mmaintz@posteo.de>
2018-02-04 20:29:46 +0100

    level 2 (intermediate)

--- rubywarrior/tarzan-intermediate/player.rb
+++ rubywarrior/tarzan-intermediate/player.rb
@@ -1,5 +1,33 @@
+require 'pry-byebug'
 class Player
+  def initialize
+    @health = 20
+  end
+
   def play_turn(warrior)
-    warrior.walk!(warrior.direction_of_stairs)
+    if !damage?(warrior) && warrior.health < 20
+      warrior.rest!
+    elsif enemy_present?(warrior)
+      warrior.attack!(locate_enemy(warrior))
+    else
+      warrior.walk!(warrior.direction_of_stairs)
+    end
+    @health = warrior.health
+  end
+
+  def damage?(warrior)
+    warrior.health < @health
+  end
+
+  def enemy_present?(warrior)
+    [:forward, :left, :right, :backward].any? do |direction|
+      warrior.feel(direction).enemy?
+    end
+  end
+
+  def locate_enemy(warrior)
+    [:forward, :left, :right, :backward].select do |direction|
+      return direction if warrior.feel(direction).enemy?
+    end
   end
 end
