03e6d0311b06cb7126da7cac586191b1a8c904ab
max <mmaintz@posteo.de>
2018-02-09 14:19:44 +0100

    level 6 (intermediate)

--- rubywarrior/tarzan-intermediate/player.rb
+++ rubywarrior/tarzan-intermediate/player.rb
@@ -1,9 +1,12 @@
 require 'pry-byebug'
 class Player
-
   def play_turn(warrior)
-    if count_enemies(warrior) > 1
-      warrior.bind!(locate(:enemy, warrior))
+    if only_attack_enemies_which_block_rescues(warrior)
+      warrior.attack!(locate(:enemy, warrior))
+    elsif only_rescue_captives_with_bombs(warrior)
+      warrior.rescue!(locate(:captive, warrior))
+    elsif ticking?(warrior)
+      warrior.walk!(direction_of_bomb(warrior))
     elsif !present?(:enemy, warrior) && warrior.health < 20
       warrior.rest!
     elsif present?(:enemy, warrior)
@@ -13,12 +16,31 @@ class Player
     elsif warrior.listen.empty?
       warrior.walk!(warrior.direction_of_stairs)
     else
-      if warrior.feel.stairs?
-        warrior.walk!(avoid_stairs(warrior))
-      else
-        warrior.walk!(warrior.direction_of(warrior.listen.first))
-      end
+      warrior.walk!(warrior.direction_of(warrior.listen.first))
     end
+  end
+
+  def only_attack_enemies_which_block_rescues(warrior)
+    if ticking?(warrior)
+      locate(:enemy, warrior) == direction_of_bomb(warrior)
+    end
+  end
+
+  def only_rescue_captives_with_bombs(warrior)
+    if ticking?(warrior)
+      locate(:captive, warrior) == direction_of_bomb(warrior)
+    end
+  end
+
+  def direction_of_bomb(warrior)
+    warrior.listen.each_with_index do |space, index|
+      @platz = index if space.ticking?
+    end
+    warrior.direction_of(warrior.listen[@platz])
+  end
+
+  def ticking?(warrior)
+    warrior.listen.any?(&:ticking?)
   end
 
   def avoid_stairs(warrior)
